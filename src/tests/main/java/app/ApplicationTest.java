edb4b4080cf0fbe55e4835d12aa0421876cee748
Dominic v. Hauff <dominic.vonhauff@gmail.com>
2022-04-05 23:48:54 +0200

    add test suite

---
+++ src/tests/main/java/app/ApplicationTest.java
@@ -0,0 +1,16 @@
+package main.java.app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+
+    /**
+     * This test embodied the pure sole of junit testing...
+     */
+    @Test
+    void aTest() {
+        assertTrue(true); // very important test
+    }
+}
