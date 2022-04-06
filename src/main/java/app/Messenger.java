cf95ca9e21ea487cc5e287e40cbf42db6e10a092
Dominic v. Hauff <dominic.vonhauff@gmail.com>
2022-04-06 21:29:03 +0200

    add file messenger, write more tests, implement messenger methods

--- src/main/java/app/Messenger.java
+++ src/main/java/app/Messenger.java
@@ -22,11 +22,55 @@ public interface Messenger {
      * @return {@code true} if the account got created successfully, {@code false} else.
      */
     boolean create(String username, String password);
+
+    /**
+     * This method provides functionality for logging into an already existing user account.
+     * @param username the username for the account to log into
+     * @param password the associated password for this account
+     * @return {@code true} if the provided args match an existing account, {@code false} else
+     */
     boolean login(String username, String password);
+
+    /**
+     * This method logs out the currently logged in account. If no user is logged in, this method does nothing.
+     */
     void logout();
+
+    /**
+     * This method can be used to send messages to other users.
+     *
+     * @param receiver the user that shall receive this method
+     * @param message the message to send as a string
+     * @return {@code true} if the message was transferred successfully, {@code false} else
+     */
     boolean send(String receiver, String message);
+
+    /**
+     * This method allows users to permanently delete received messages in their mailbox.
+     *
+     * @param index The message index inside their mailbox
+     */
     void delete(int index);
+
+    /**
+     * This method creates a list of all messages inside the mailbox of a user.
+     *
+     * @return a {@link List} of all messages inside the mailbox of the logged-in user
+     */
     List<Message> list();
+
+    /**
+     * This method creates a list of all unread messages inside the mailbox of a user.
+     *
+     * @return a {@link List} of all unread messages inside the mailbox of the logged-in user
+     */
     List<Message> listUnread();
+
+    /**
+     * This method allows a logged-in user to read a single message stored in their mailbox.
+     *
+     * @param index the index of the message inside their mailbox the user wants to read
+     * @return the message selected by the provided index
+     */
     Message read(int index);
 }
--- src/main/java/app/SimpleMessenger.java
+++ src/main/java/app/SimpleMessenger.java
@@ -6,6 +6,8 @@ import app.util.FileManager;
 import java.util.List;
 
 public class SimpleMessenger implements Messenger {
+    private static final String PATH_TO_USER_DIRS = "src/resources/users/";
+
     /**
      * This method provides functionality for creating a new user account for this messaging application
      * Note that the username functions as a unique identifier thus there cannot be another account with the same
@@ -20,36 +22,73 @@ public class SimpleMessenger implements Messenger {
         return FileManager.createDir(PATH_TO_USER_DIRS + username);
     }
 
+    /**
+     * This method provides functionality for logging into an already existing user account.
+     * @param username the username for the account to log into
+     * @param password the associated password for this account
+     * @return {@code true} if the provided args match an existing account, {@code false} else
+     */
     @Override
     public boolean login(String username, String password) {
         return false;
     }
 
+    /**
+     * This method logs out the currently logged in account. If no user is logged in, this method does nothing.
+     */
     @Override
     public void logout() {
 
     }
 
+    /**
+     * This method can be used to send messages to other users.
+     *
+     * @param receiver the user that shall receive this method
+     * @param message the message to send as a string
+     * @return {@code true} if the message was transferred successfully, {@code false} else
+     */
     @Override
     public boolean send(String receiver, String message) {
         return false;
     }
 
+    /**
+     * This method allows users to permanently delete received messages in their mailbox.
+     *
+     * @param index The message index inside their mailbox
+     */
     @Override
     public void delete(int index) {
 
     }
 
+    /**
+     * This method creates a list of all messages inside the mailbox of a user.
+     *
+     * @return a {@link List} of all messages inside the mailbox of the logged-in user
+     */
     @Override
     public List<Message> list() {
         return null;
     }
 
+    /**
+     * This method creates a list of all unread messages inside the mailbox of a user.
+     *
+     * @return a {@link List} of all unread messages inside the mailbox of the logged-in user
+     */
     @Override
     public List<Message> listUnread() {
         return null;
     }
 
+    /**
+     * This method allows a logged-in user to read a single message stored in their mailbox.
+     *
+     * @param index the index of the message inside their mailbox the user wants to read
+     * @return the message selected by the provided index
+     */
     @Override
     public Message read(int index) {
         return null;
